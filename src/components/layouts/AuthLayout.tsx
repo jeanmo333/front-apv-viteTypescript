5b62f18fbc02fbc0e1057cfc0fd92d5aba5069a9
Moril <jeanmo333@gmail.com>
2023-04-03 21:25:49 -0400

    commit

--- src/components/layouts/AuthLayout.tsx
+++ src/components/layouts/AuthLayout.tsx
@@ -1,3 +1,5 @@
+/** @format */
+
 // import { FC} from 'react';
 // import { Box } from '@mui/material';
 
@@ -6,16 +8,23 @@
 //     children: React.ReactNode;
 // }
 //import { Box } from "@mui/material";
-import { Outlet } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
+import { useAuth } from "../../hooks/useAuth";
 
 export const AuthLayout = () => {
+  const { auth, loading } = useAuth();
+  const token = localStorage.getItem("token");
   return (
     <>
-      <main className="container mx-auto md:mt-6 p-5  md:flex md:justify-center">
-        <div className="md:w-2/3 lg:w-2/5 ">
-          <Outlet />
-        </div>
-      </main>
+      {auth?._id || token ? (
+        <Navigate to='/admin' />
+      ) : (
+        <main className='container mx-auto md:mt-6 p-5  md:flex md:justify-center'>
+          <div className='md:w-2/3 lg:w-2/5 '>
+            <Outlet />
+          </div>
+        </main>
+      )}
 
       {/* <main>
         <Box
